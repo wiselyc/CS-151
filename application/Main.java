d485f6099feea0b4e58712ac531214790b8b298f
sarahmac2000 <93111127+sarahmac2000@users.noreply.github.com>
2021-11-09 23:40:19 -0800

    Add files via upload

---
+++ application/Main.java
@@ -0,0 +1,41 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	
+	private static Stage stg;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+			
+			stg = primaryStage;
+			primaryStage.setResizable(false);
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("InputTransaction.fxml"));
+			Scene scene = new Scene(root,600,600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("MoMuney");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
