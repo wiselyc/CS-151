396e851d077a7481ddf548f91db84e75f896065d
wiselyc <89878777+wiselyc@users.noreply.github.com>
2021-11-10 17:17:59 -0800

    Momoney menu

---
+++ SampleController.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SampleController {
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchtoMenu(ActionEvent event) throws IOException
+	{
+		root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchtoInput(ActionEvent event) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("Input.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchtoPast(ActionEvent event) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("Past.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchtoSummary(ActionEvent event) throws IOException
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("Summary.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
+}
